e48e62e043f87eeab883ab06f5a640562d2d0c91
Alex Villegas <avillegas@dragonspears.com>
2023-03-06 21:09:03 -0600

    First Commit - test

---
+++ Clase1/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Transporte
+{
+    public virtual void Dibujar()
+    {
+        Console.WriteLine("Dibujando una figura genérica");
+    }
+}
+
+public class Avion : Transporte
+{
+    public override void Dibujar()
+    {
+        Console.WriteLine("Dibujando un triángulo");
+    }
+}
+
+public class Caballo : Transporte
+{
+    public override void Dibujar()
+    {
+        Console.WriteLine("Dibujando un círculo");
+    }
+}
+
+public class Carro : Transporte
+{
+    public override void Dibujar()
+    {
+        Console.WriteLine("Dibujando un cuadrado");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Transporte[] figuras = new Transporte[3];
+
+        figuras[0] = new Avion();
+        figuras[1] = new Caballo();
+        figuras[2] = new Carro();
+
+        foreach (Transporte figura in figuras)
+        {
+            figura.Dibujar();
+        }
+    }
+}
