5d5f0bd609e9fc4c98e708a232b772e5d74f4c31
Alex Villegas <avillegas@dragonspears.com>
2023-03-06 21:48:24 -0600

    New project Structure

---
+++ Clase1/Lab1/AlexVillegas.cs
@@ -0,0 +1,51 @@
+﻿namespace Clase1.Lab1;
+
+public class Transporte
+{
+    public virtual void Dibujar()
+    {
+        Console.WriteLine("Dibujando una figura genérica");
+    }
+}
+
+public class Avion : Transporte
+{
+    public override void Dibujar()
+    {
+        Console.WriteLine("Dibujando un triángulo");
+    }
+}
+
+public class Caballo : Transporte
+{
+    public override void Dibujar()
+    {
+        Console.WriteLine("Dibujando un círculo");
+    }
+}
+
+public class Carro : Transporte
+{
+    public override void Dibujar()
+    {
+        Console.WriteLine("Dibujando un cuadrado");
+    }
+}
+
+internal class AlexVillegas
+{
+    public static void Run()
+    {
+        // Alex VC - 503990937
+        Transporte[] transporte = new Transporte[3];
+
+        transporte[0] = new Avion();
+        transporte[1] = new Caballo();
+        transporte[2] = new Carro();
+
+        foreach (Transporte figura in transporte)
+        {
+            figura.Dibujar();
+        }
+    }
+}
