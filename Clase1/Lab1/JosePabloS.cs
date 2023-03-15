6451b818095514c9ae3167a7200f870ee5623422
José Pablo <suarezgomezjosepablo03@gmail.com>
2023-03-14 20:22:30 -0600

    Tarea Progra

---
+++ Clase1/Lab1/JosePabloS.cs
@@ -0,0 +1,56 @@
+﻿namespace Clase1.Lab1;
+
+public class Persona 
+{
+    public virtual void Saludar()
+    {
+        Console.WriteLine("Saludar ");
+    }
+}
+
+public class Profesor : Persona
+{
+    public override void Saludar ()
+    {
+        Console.WriteLine("Hola soy el profesor!");
+    }
+}
+
+public class Estudiante : Persona
+{
+    public override void Saludar()
+    {
+        Console.WriteLine("Hola soy el estudiante!");
+    }
+}
+
+public class Director : Persona
+{
+    public override void Saludar()
+    {
+        Console.WriteLine("Hola soy el director!");
+    }
+}
+
+internal class JosePablo
+{
+    public static void Run()
+    {
+        Persona[] personas = new Persona[3];
+
+        personas[0] = new Profesor();
+        personas[1] = new Estudiante();
+        personas [2] = new Director ();
+
+        foreach (Persona persona in personas)
+        {
+            persona.Saludar();
+        }
+        
+        Console.ReadKey();
+    }
+}
+
+
+
+
--- Clase1/Program.cs
+++ Clase1/Program.cs
@@ -2,3 +2,4 @@
 
 AlexVillegas.Run();
 RonaldoMora.Run();
+JosePablo.Run();
