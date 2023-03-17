f0b6c534189b63001cfe22475c92a1adda1df133
otomora <erickmora1436@gmail.com>
2023-03-17 07:55:34 +0100

    Tarea 2

---
+++ Clase1/Lab2/MotocicletaBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Clase2.Lab1
+{
+    public class MotocicletaBase
+    {
+        public override void Avanzar(int velocidad = 0)
+        {
+            velocidad = velocidad * 5;
+            Console.WriteLine("Volando a velocidad: " + velocidad);
+        }
+    }
+}
