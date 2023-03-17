93bd8ee93dd6a0cef3e896ce2745b58372fbc903
YoilinCastrillo <yoilinc7alfaro@gmail.com>
2023-03-17 16:27:45 -0600

    clase

--- Clase1/Program.cs
+++ Clase1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Lab1 = Clase1.Lab1;
-using Lab2 = Clase1.Lab2;
+using Lab2 = Clase1.Lab2; 
 
 #region Lab1;
 
@@ -10,7 +10,7 @@ Lab1.JosePablo.Run();
 Lab1.ErickMora.Run();
 Lab1.AshlyVargas.Run();
 Lab1.AaronChaves.Run();
-
+Lab1.YoilinCastrillo.Run();
 #endregion Lab1;
 
 
@@ -18,5 +18,5 @@ Lab1.AaronChaves.Run();
 
 /// Sobrecarga de métodos y sobre escritura.
 Lab2.AlexVillegas.Run();
-
+Lab2.YoilinCastrillo.Run();
 #endregion Lab2;
