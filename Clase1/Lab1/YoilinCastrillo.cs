e522e5b84d9a4f5db1bfa752d10e022de9a14316
Alex Villegas <avillegas@dragonspears.com>
2023-03-31 15:11:53 -0600

    Fixing Merge Issues

--- Clase1/Lab1/YoilinCastrillo.cs
+++ Clase1/Lab1/YoilinCastrillo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Clase1.Lab1
+﻿namespace Labs.Lab1
 {
     public class Especie
     {
--- Clase1/Lab2/YoilinCastrillo.cs
+++ Clase1/Lab2/YoilinCastrillo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Clase1.Lab2
+﻿namespace Labs.Lab2
 {
     public class Especie
     {
