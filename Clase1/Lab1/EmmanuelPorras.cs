17e7552eb356d5b2c3f94306505078af47fb3ae0
Emmanuel Porras <eporrascastro2004@gmail.com>
2023-04-01 16:43:00 -0600

    Labs

---
+++ Clase1/Lab1/EmmanuelPorras.cs
@@ -0,0 +1,53 @@
+﻿namespace Labs1.Lab1;
+
+using Labs.Lab2;
+using System;
+
+public class LineasTelefonicas
+{
+    public virtual void Plan()
+    {
+        Console.WriteLine("Plan Base");
+    }
+}
+
+public class Claro : LineasTelefonicas
+{
+    public override void Plan()
+    {
+        Console.WriteLine("Plan Claro");
+    }
+}
+
+public class Movistar : LineasTelefonicas
+{
+    public override void Plan()
+    {
+        Console.WriteLine("Plan Movistar");
+    }
+}
+
+public class Kolbi : LineasTelefonicas
+{
+    public override void Plan()
+    {
+        Console.WriteLine("Plan Kolbi");
+    }
+}
+//Test
+public class EmmanuelPorras
+{
+    public static void Run()
+    {
+        LineasTelefonicas[] transportes = new LineasTelefonicas[3];
+
+        transportes[0] = new Claro();
+        transportes[1] = new Movistar();
+        transportes[2] = new Kolbi();
+
+
+
+    }
+
+
+}
