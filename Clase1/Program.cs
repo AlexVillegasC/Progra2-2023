f6d153de35ea1f907df84890cd903491c5a353ba
Alex Villegas <avillegas@dragonspears.com>
2023-04-11 20:05:34 -0600

    Code Clean Up

--- Clase1/Program.cs
+++ Clase1/Program.cs
@@ -1,33 +1,4 @@
-﻿using Lab1 = Labs.Lab1;
-using Lab2 = Labs.Lab2;
+﻿using Lab4 = Labs.Lab4;
 
-#region Lab1;
-
-/// Polimorfismo.
-Lab1.AlexVillegas.Run();
-Lab1.RonaldoMora.Run();
-Lab1.JosePablo.Run();
-Lab1.ErickMora.Run();
-Lab1.AshlyVargas.Run();
-Lab1.AaronChavesB.Run();
-Lab1.AkionCheng.Run();
-Lab1.LeonardoZuniga.Run();
-Lab1.JosueFernandez.Run();
-Lab1.WilsonChavarria.Run(); 
-#endregion Lab1;
-
-
-#region Lab2;
-
-/// Sobrecarga de métodos y sobre escritura.
-Lab2.AlexVillegas.Run();
-Lab2.AaronChaves.Run();
-Lab2.ErickMora.Run();
-Lab2.JosePabloS_Sobrecaga.Run();
-Lab2.LeonardoZuniga.Run();
-Lab2.AkionCheng.Run();
-Lab2.RonaldoMora.Run();
-// Lab2.WilsonChavarria.Run(); -- // Reparar.
-Lab2.JOSUE_fernandez.Run();
-
-#endregion Lab2;
+// No agregar código aquí. Solamente utilizar esta clase para probar!
+Lab4.AlexVillegasProgram.Run();
