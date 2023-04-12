922094c20ec2e8bef7da4338eb11cdf9075d5934
Alex Villegas <avillegas@dragonspears.com>
2023-04-11 20:53:26 -0600

    Lab5 Interfaces + Herencia Multiple

--- Clase1/Program.cs
+++ Clase1/Program.cs
@@ -1,3 +1,6 @@
-﻿using Lab4 = Labs.Lab4;
+﻿using Lab1 = Labs.Lab1;
+using Lab2 = Labs.Lab2;
+using Lab4 = Labs.Lab4;
+using Lab5 = Labs.Lab5;
 
 // No agregar código aquí. Solamente utilizar esta clase para probar!
