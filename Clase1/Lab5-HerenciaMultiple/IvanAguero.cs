892e08fbeff45ae50a037978dc3a57c70141c4ff
1van2004 <ivanaa2004@hotmail.com>
2023-04-14 15:29:50 -0600

    Lab5

---
+++ Clase1/Lab5-HerenciaMultiple/IvanAguero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs.Lab5_HerenciaMultiple
+{
+    public interface ICorredor
+    {
+        void Correr();
+    }
+
+    public interface IComedor
+    {
+        void Comer();
+    }
+
+    public class Pantera : ICorredor, IComedor
+    {
+        public void Correr()
+        {
+            Console.WriteLine("La pantera está corriendo.");
+        }
+
+        public void Comer()
+        {
+            Console.WriteLine("La pantera esta comiendo.");
+        }
+    }
+
+    internal class IvanAgueroP
+    {
+        public static void Run()
+        {
+            Pantera Rosa = new Pantera();
+            Rosa.Correr();
+            Rosa.Comer();
+        }
+    }
+
+}
