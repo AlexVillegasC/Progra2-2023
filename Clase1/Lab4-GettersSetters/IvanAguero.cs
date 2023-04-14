e14653cc4a9aa6f7ea8781eab5356e3e423216eb
1van2004 <ivanaa2004@hotmail.com>
2023-04-14 14:57:52 -0600

    Lab4

--- Clase1/Lab4-GettersSetters/IvanAguero.cs
+++ Clase1/Lab4-GettersSetters/IvanAguero.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace Labs.Lab4_GettersSetters
 {
-    internal class IvanAguero
+    public class Estudiante
     {
+        private string _Nombre;
+        private string _Carrera;
+
+
+        public Estudiante() { }
+
+        public Estudiante(string Nombre, string Carrera)
+        {
+            _Nombre = Nombre;
+            _Carrera = Carrera;
+        }
+
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+
+        public string Carrera
+        {
+            get { return _Carrera; }
+
+            set
+            {
+                if (!value.Equals(""))
+                {
+                    _Carrera = value;
+                }
+            }
+        }
     }
+
+    public class IvanAgueroP
+    {
+        public static void Run()
+        {
+            // Ejemplo 1:
+            Estudiante estudiante1 = new Estudiante();
+            estudiante1.Nombre = "Julio";
+            estudiante1.Carrera = "Turismo";
+            Console.WriteLine("Nombre del estudiante: " + estudiante1.Nombre);
+            Console.WriteLine("Carrera de estudiante : " + estudiante1.Carrera);
+
+            // Ejemplo 2:
+            Estudiante estudiante2 = new Estudiante("Maria", "Ingenieria");
+            Console.WriteLine("Nombre del estudiante: " + estudiante2.Nombre);
+            Console.WriteLine("Carrera del estudiante: " + estudiante2.Carrera);
+        }
+    }
+
 }
