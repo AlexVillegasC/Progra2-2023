811c3472561e899ac4aae99c9721d45ba92b9a08
gabogj03 <gabogonzarez@gmail.com>
2023-04-14 03:26:35 +0200

    1change

---
+++ Clase1/Lab2-SobreCarga/GabrielGonzalez.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Ejercicio
+{
+    public virtual void Entrenar()
+    {
+        Console.WriteLine("Entrenando");
+    }
+}
+
+class Gym : Ejercicio
+{
+    public override void Entrenar()
+    {
+        Console.WriteLine("Haciendo pesas en el gym");
+    }
+}
+
+class Crossfit : Ejercicio
+{
+    public override void Entrenar()
+    {
+        Console.WriteLine("Haciendo WODs en crossfit");
+    }
+}
+
+class Calistenia : Ejercicio
+{
+    public override void Entrenar()
+    {
+        Console.WriteLine("Haciendo ejercicios de calistenia");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ejercicio ej = new Ejercicio();
+        ej.Entrenar();
+
+        Gym gym = new Gym();
+        gym.Entrenar();
+
+        Crossfit crossfit = new Crossfit();
+        crossfit.Entrenar();
+
+        Calistenia calistenia = new Calistenia();
+        calistenia.Entrenar();
+    }
+}
