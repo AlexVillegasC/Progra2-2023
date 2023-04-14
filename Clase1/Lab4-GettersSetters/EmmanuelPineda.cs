9be463a6ac7dd295e370e7c42e0e6d3c1ac76e1b
PinedaCR10 <emmanuelplay2233@gmail.com>
2023-04-14 15:30:40 -0600

    Laboratorios 4 y 5

---
+++ Clase1/Lab4-GettersSetters/EmmanuelPineda.cs
@@ -0,0 +1,53 @@
+namespace Labs.Lab4;
+
+public class Person
+{
+    private string _cedula;
+    private string _numeroTelefonico;
+
+
+    public Person() { }
+
+    public Person(string cedula, string numeroTelefonico)
+    {
+        _cedula = cedula;
+        _numeroTelefonico = numeroTelefonico;
+    }
+
+    public string cedula
+    {
+        get { return _cedula; }
+        set { _cedula = value; }
+    }
+
+    public string numeroTelefonico
+    {
+        get { return _numeroTelefonico; }
+
+        set
+        {
+            if (!value.Equals(""))
+            {
+                _numeroTelefonico = value;
+            }
+        }
+    }
+}
+
+public class EmmanuelPinedaProgram
+{
+    public static void Run()
+    {
+        // 1st Example:
+        Person person1 = new Person();
+        person1.cedula = "504480290";
+        person1.numeroTelefonico = "85042171";
+        Console.WriteLine("cedula: " + person1.cedula);
+        Console.WriteLine("numeroTelefonico: " + person1.numeroTelefonico);
+
+        // 2nd Example:
+        Person person2 = new Person("Emmanuel", "Pineda");
+        Console.WriteLine("cedula: " + person2.cedula);
+        Console.WriteLine("numeroTelefonico: " + person2.numeroTelefonico);
+    }
+}
