42816a59ae784ced09fb31a3eece1adffe97f00c
pity05 <diazpri4@gmail.com>
2023-04-14 15:43:43 -0600

    Labs

---
+++ Clase1/Lab4-GettersSetters/PriscilaDiaz4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs.Lab4_GettersSetters
+{
+    internal class PriscilaDiaz4
+    {
+    }
+}
