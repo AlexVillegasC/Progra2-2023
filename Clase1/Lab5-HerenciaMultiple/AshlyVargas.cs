fa4264ec6e0879cb560d04119ed50fb5a41379ef
AshVargas <vargasashly260@gmail.com>
2023-04-14 15:37:30 -0600

    Laboratorio

---
+++ Clase1/Lab5-HerenciaMultiple/AshlyVargas.cs
@@ -0,0 +1,36 @@
+﻿namespace Labs.Lab5;
+
+public interface ICiclista
+{
+    void Pedalear();
+}
+
+public interface IProfesor
+
+{
+    void Enseñar();
+}
+
+public class Persona : ICiclista, IProfesor
+{
+    public void Pedalear()
+    {
+        Console.WriteLine("La persona está pedaleando.");
+    }
+
+    public void Enseñar()
+    {
+        Console.WriteLine("La persona está enseñando.");
+    }
+}
+
+internal class AshlyVargasProgram
+{
+    public static void Run()
+    {
+        Persona Nixon = new Persona();
+        Nixon.Pedalear();
+        Nixon.Enseñar();
+
+    }
+}
