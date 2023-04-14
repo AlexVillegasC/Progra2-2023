981a4f80d80e7ed0cb2bdcb5a1dc655e2eef52ef
YoilinCastrillo <yoilinc7alfaro@gmail.com>
2023-04-14 15:37:18 -0600

    L

---
+++ Clase1/Lab5-HerenciaMultiple/Yoilin Castrillo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace EjemploHerenciaMultiple
+{
+    public interface IMovil
+    {
+        void Mover();
+    }
+
+    public interface ISonoro
+    {
+        void HacerSonido();
+    }
+
+    public class Coche : IMovil, ISonoro
+    {
+        public void Mover()
+        {
+            Console.WriteLine("El coche está en movimiento.");
+        }
+
+        public void HacerSonido()
+        {
+            Console.WriteLine("El coche está haciendo un sonido.");
+        }
+    }
+
+    public class Avion : IMovil, ISonoro
+    {
+        public void Mover()
+        {
+            Console.WriteLine("El avión está en el aire.");
+        }
+
+        public void HacerSonido()
+        {
+            Console.WriteLine("El avión está haciendo un ruido.");
+        }
+
+        public void Aterrizar()
+        {
+            Console.WriteLine("El avión está aterrizando.");
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            Coche miCoche = new Coche();
+            miCoche.Mover();
+            miCoche.HacerSonido();
+
+            Avion miAvion = new Avion();
+            miAvion.Mover();
+            miAvion.HacerSonido();
+            miAvion.Aterrizar();
+        }
+    }
+}
