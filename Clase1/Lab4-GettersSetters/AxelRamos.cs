70ff87e03e665d2f0da905537719488a4438043b
Axel Ramos <axelramos028@gmail.com>
2023-04-14 15:31:33 -0600

    lab 4 y 5

---
+++ Clase1/Lab4-GettersSetters/AxelRamos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+public class Sujeto
+{
+    private string _firstName;
+    private string _lastName;
+
+    public Sujeto() { }
+
+    public Sujeto(string firstName, string lastName)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+    }
+
+    public string FirstName
+    {
+        get { return _firstName; }
+        set { _firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return _lastName; }
+
+        set
+        {
+            if (!value.Equals(""))
+            {
+                _lastName = value;
+            }
+        }
+    }
+}
+
+public class MyProgram
+{
+    public static void Run()
+    {
+        // 1st Example:
+        Sujeto person1 = new Sujeto();
+        person1.FirstName = "Emily";
+        person1.LastName = "Johnson";
+        Console.WriteLine("First Name: " + person1.FirstName);
+        Console.WriteLine("Last Name: " + person1.LastName);
+
+        // 2nd Example:
+        Sujeto person2 = new Sujeto("David", "Smith");
+        Console.WriteLine("First Name: " + person2.FirstName);
+        Console.WriteLine("Last Name: " + person2.LastName);
+    }
+}
