8a0868a51f64172daff6bbae4ad5c283f2687e25
Pablo Obando <pabloobando03@gmail.com>
2023-04-22 14:06:10 -0600

    Lab-4 - 5

---
+++ Clase1/Lab5-HerenciaMultiple/PabloObando.cs
@@ -0,0 +1,34 @@
+﻿namespace Labs.Lab5;
+
+public interface ILadrador
+{
+    void Ladrar();
+}
+
+public interface IMordedor
+{
+    void Morder();
+}
+
+public class Perro : ILadrador, IMordedor
+{
+    public void Ladrar()
+    {
+        Console.WriteLine("El perro esta ladrando.");
+    }
+
+    public void Morder()
+    {
+        Console.WriteLine("El perro esta mordiendo.");
+    }
+}
+
+internal class PabloObando
+{
+    public static void Run()
+    {
+        Perro Dante = new Perro();
+        Dante.Ladrar();
+        Dante.Morder();
+    }
+}
