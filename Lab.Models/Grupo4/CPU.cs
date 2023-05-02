5c80f72827c0ce04f69f7eba2b8e536e8d7c8458
Emmanuel Porras <eporrascastro2004@gmail.com>
2023-05-02 09:16:32 -0600

    CPU class

--- Lab.Models/Grupo4/CPU.cs
+++ Lab.Models/Grupo4/CPU.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab.Models.Grupo4
+namespace Lab.Models.Grupo4;
+class CPU
 {
-    internal class CPU
+    private Motherboard motherboard;
+    private RAM ram;
+    private HardDrive hardDrive;
+    private PowerSupply powerSupply;
+
+    public CPU(Motherboard motherboard, RAM ram, HardDrive hardDrive, PowerSupply powerSupply)
     {
+        this.motherboard = motherboard;
+        this.ram = ram;
+        this.hardDrive = hardDrive;
+        this.powerSupply = powerSupply;
+    }
+
+    public void Start()
+    {
+        powerSupply.TurnOn();
+        motherboard.PowerOn();
+        ram.LoadBootProgram(hardDrive.ReadBootSector());
+        motherboard.ExecuteProgram();
+    }
+
+    public void ShutDown()
+    {
+        motherboard.PowerOff();
+        powerSupply.TurnOff();
     }
 }
