ba2b365f010e7a123c0efe9aa8c84efec28e0b8d
Emmanuel Porras <eporrascastro2004@gmail.com>
2023-05-02 12:42:35 -0600

    Nueva version de codigo

--- Lab.Models/Grupo4/CPU.cs
+++ Lab.Models/Grupo4/CPU.cs
@@ -1,37 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
+using static Lab.Models.Grupo4.CPU;
 
 namespace Lab.Models.Grupo4;
-class CPU
-{
-    private Motherboard motherboard;
-    private RAM ram;
-    private HardDrive hardDrive;
-    private PowerSupply powerSupply;
 
-    public CPU(Motherboard motherboard, RAM ram, HardDrive hardDrive, PowerSupply powerSupply)
+
+public class CPU
+
+{
+    private Componentes componentes;
+
+    public CPU(Componentes componentes)
     {
-        this.motherboard = motherboard;
-        this.ram = ram;
-        this.hardDrive = hardDrive;
-        this.powerSupply = powerSupply;
+        this.componentes = componentes;
     }
 
     public void Start()
     {
-        powerSupply.TurnOn();
-        motherboard.PowerOn();
-        ram.LoadBootProgram(hardDrive.ReadBootSector());
-        motherboard.ExecuteProgram();
+        // Inicia el encendido del CPU
     }
 
     public void ShutDown()
     {
-        motherboard.PowerOff();
-        powerSupply.TurnOff();
+        // Apaga el CPU
+    }
+
+    public class Componentes
+    {
+        public string motherboard;
+        public string ram;
+        public string hardDrive;
+        public string powerSupply;
     }
 }
