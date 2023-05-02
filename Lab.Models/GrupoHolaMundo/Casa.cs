561c272a11f5f0a01eac35f80127ccc0c50eab73
Ronaldo Mora <ronaldo.mora.diaz@est.una.ac.cr>
2023-05-01 21:50:12 -0600

    Se agregó la clase generica "Casa"

---
+++ Lab.Models/GrupoHolaMundo/Casa.cs
@@ -0,0 +1,43 @@
+﻿public class Casa
+{
+    private int habitaciones;
+    private int banos;
+    private int comedor;
+    private int piscina;
+
+    public Casa()
+    {
+    }
+
+    public Casa(int habitaciones, int banos, int comedor, int piscina)
+    {
+        this.habitaciones = habitaciones;
+        this.banos = banos;
+        this.comedor = comedor;
+        this.piscina = piscina;
+    }
+
+    public int Habitaciones
+    {
+        get { return habitaciones; }
+        set { habitaciones = value; }
+    }
+
+    public int Banos
+    {
+        get { return banos; }
+        set { banos = value; }
+    }
+
+    public int Comedor
+    {
+        get { return comedor; }
+        set { comedor = value; }
+    }
+
+    public int Piscina
+    {
+        get { return piscina; }
+        set { piscina = value; }
+    }
+}
