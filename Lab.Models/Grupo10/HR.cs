726f83a6c1f2cc9d5270341e4eef6a0934464439
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2023-05-04 09:26:05 -0600

    Se agrego el UML

--- Lab.Models/Grupo10/HR.cs
+++ Lab.Models/Grupo10/HR.cs
@@ -2,5 +2,41 @@
 
 public class HR
 {
+    public string Nombre;
+    public List<Empleado> Empleados;
+    public Empleado Gerente;
+    public string Ubicacion;
+     
+    public void Contratar(Empleado empleado)
+    {
+        Empleados.Add(empleado);
+    }
     
+    public void Despedir(Empleado empleado)
+    {
+        Empleados.Remove(empleado);
+    }
+    
+    public void Promover(Empleado empleado)
+    {
+        Gerente = empleado;
+    }
+    
+    public void CambiarUbicacion(string ubicacion)
+    {
+        Ubicacion = ubicacion;
+    }
+    
+    public List<Empleado> ObtenerEmpleados()
+    {
+        return Empleados;
+    }
+    
+    public class Empleado
+    {
+        public string nombre;
+        public int edad;
+        public string puesto;
+        public float salario;
+    }
 }
