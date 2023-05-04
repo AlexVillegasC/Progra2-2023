8906cf31cdb9f2e1c29e2013d2349c3005c18476
YoilinCastrillo <yoilinc7alfaro@gmail.com>
2023-05-04 11:03:50 -0600

    se agrega clase case

--- Lab.Models/grupo4/Case.cs
+++ Lab.Models/grupo4/Case.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Lab.Models.Grupo4
 {
-    internal class Case
+    public class Case
     {
+        int Id;
+        string Nombre;
+        double Tamanno;
+        string Color;
+        decimal Costo;
+
+        public Case(int id, string nombre, double tamanno, string color, decimal costo)
+        {
+            this.Id = id;
+            Nombre = nombre;
+            Tamanno = tamanno;
+            Color = color;
+            Costo = costo;
+        }
+        public int getId() { return Id; }
+        public string getNombre() {  return Nombre; }
+        public double getTamanno() { return Tamanno; }
+        public string getColor() { return Color; }
+        public decimal getCosto() {  return Costo; }
+
+        public void setId(int id) { this.Id = id; }
+        public void setNombre(string nombre) { this.Nombre = nombre; }
+        public void setTamanno(double tamanno) { this.Tamanno = tamanno; }
+        public void setColor(string color) {  this.Color = color; }
+        public void setCosto(decimal costo) { this.Costo = costo; }
+
+
+
     }
 }
