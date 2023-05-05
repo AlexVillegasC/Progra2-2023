9395f9c7f9d02b01a470e658c9e78e038beb71e5
YoilinCastrillo <yoilinc7alfaro@gmail.com>
2023-05-05 13:53:35 -0600

    agregue ¨Forma¨

---
+++ Lab.Models/Grupo4/Case.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Models.Grupo4
+{
+    public class Case
+    {
+        int Id;
+        string Nombre;
+        double Tamanno;
+        string Color;
+        decimal Costo;
+        string Forma;
+
+        public Case(int id, string nombre, double tamanno, string color, decimal costo, string forma)
+        {
+            this.Id = id;
+            Nombre = nombre;
+            Tamanno = tamanno;
+            Color = color;
+            Costo = costo;
+            Forma = forma;
+        }
+        public int getId() { return Id; }
+        public string getNombre() {  return Nombre; }
+        public double getTamanno() { return Tamanno; }
+        public string getColor() { return Color; }
+        public decimal getCosto() {  return Costo; }
+        public string getForma() {  return Forma; }
+
+        public void setId(int id) { this.Id = id; }
+        public void setNombre(string nombre) { this.Nombre = nombre; }
+        public void setTamanno(double tamanno) { this.Tamanno = tamanno; }
+        public void setColor(string color) {  this.Color = color; }
+        public void setCosto(decimal costo) { this.Costo = costo; }
+        public void setForma(string forma) { this.Forma = forma; }
+
+
+
+    }
+}
