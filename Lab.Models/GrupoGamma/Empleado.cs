a0511c2871821e435da7428bc9893f9132332af4
OAiza <power777pro@gmail.com>
2023-05-04 22:41:40 -0600

    Se establecen atributos de la clase empleado

--- Lab.Models/GrupoGamma/Empleado.cs
+++ Lab.Models/GrupoGamma/Empleado.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Lab.Models.GrupoGamma
 {
-    internal class Empleado
+    public class Empleado
     {
+        private int id;
+        private string Nombre_emp;
+        private int Telefono_emp;
+        private string campo;
+
+
+
+
+
     }
 }
