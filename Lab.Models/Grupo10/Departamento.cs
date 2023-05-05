b2cb18fbccdcbecbca8d347f1584bcc276108417
Aaron Chaves UNA <achaba03@gmail.com>
2023-05-05 00:08:02 -0600

    avance

--- Lab.Models/Grupo10/Departamento.cs
+++ Lab.Models/Grupo10/Departamento.cs
@@ -1,6 +1,27 @@
 ﻿namespace Lab.Models.Grupo10;
+using System.Collections.Generic;
 
 public class Departamento
 {
+    private List<Empleado> empleados;
     
-}
+    public string NombreDepartamento;
+        public string IdDepartamento;
+
+        public Departamento() {
+            empleados = new List<Empleado>();
+        }
+
+        public void agregar_empleado(Empleado empleado) {
+            empleados.Add(empleado);
+        }
+
+    
+        public void eliminar_empleado(Empleado empleado) {
+            empleados.Remove(empleado);
+        }
+
+        public List<Empleado> obtener_empleados() {
+            return empleados;
+        }
+    }
