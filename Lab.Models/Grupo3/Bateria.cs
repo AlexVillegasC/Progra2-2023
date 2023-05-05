28608568e53cda1cec45658adedd9e45faf7a7e2
WILSON CHAVARRIA RUIZ <wilson.chavarria.ruiz@est.una.ac.cr>
2023-05-05 17:45:21 -0600

    proyecto

---
+++ Lab.Models/Grupo3/Bateria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Models.Grupo3
+{
+    
+    
+        public class Bateria
+        {
+            public int Capacidad { get; set; }
+          
+            public bool Estado { get; set; }
+            
+
+              
+            
+        }
+    
+}
---
+++ Lab.Models/Grupo3/celular.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Models.Grupo3
+{
+    internal class celular
+    {
+        public Bateria bateria { get; set; }
+        public Memoria memoria { get; set; }
+        public TarjetaSim tarjetaSim { get; set; }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }  
+        public string Color { get; set; }
+    }   
+}
