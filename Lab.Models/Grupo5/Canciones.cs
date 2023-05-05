806a2078e4380cdde07c4d014f0ebede0b14397c
JoseBaltodanoH <joseabh2004@gmail.com>
2023-05-05 15:32:51 -0600

    N canciones

--- Lab.Models/Grupo5/Canciones.cs
+++ Lab.Models/Grupo5/Canciones.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab.Models.Grupo5
+namespace Lab.Models.Grupo5;
+
+
+class Canciones
 {
-    internal class Canciones
+    private List<string> listaCanciones = new List<string>();
+    private List<Album> listaAlbums = new List<Album>();
+    private List<Artista> listaArtistas = new List<Artista>();
+    private List<Letra> listaLetras = new List<Letra>();
+
+    public void AgregarCancion(string nombreCancion, Album album, Artista artista, Letra letra)
     {
+        listaCanciones.Add(nombreCancion);
+        listaAlbums.Add(album);
+        listaArtistas.Add(artista);
+        listaLetras.Add(letra);
+    }
+
+    public void VerListaCanciones()
+    {
+        for (int i = 0; i < listaCanciones.Count; i++)
+        {
+            Console.WriteLine($"Canción: {listaCanciones[i]}, Álbum: {listaAlbums[i].NombreAlbum}, Artista: {listaArtistas[i].NombreArtista}, Letra: {listaLetras[i].Contenido}");
+        }
     }
 }
