033c75208711d962186297c9a8caed3a82a4eb77
gabogj03 <gabogonzarez@gmail.com>
2023-05-05 15:53:04 -0600

    eeeee

--- Lab.Models/Grupo5/letra.cs
+++ Lab.Models/Grupo5/letra.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab.Models.Grupo5
+namespace Lab.Models.Grupo5;
+class Letra : Canciones
 {
-    internal class letra
-    {
-
-    }
+    public string Contenido { get; set; }
 }
+
