78fd9efe7e8b266fd53f974d9993d9865cb8ac64
SofiaSJ09 <sjimenezsofia@gmail.com>
2023-05-10 22:54:25 -0600

    Clases

--- Lab.Models/Grupo7/Libro2.0.cs
+++ Lab.Models/Grupo7/Libro2.0.cs
@@ -1,95 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lab.Models.Grupo7
 {
-    internal class Libro
-    {
-    }
+
+        public class Libro
+        {
+            private int id;
+            private string categoria;
+            private string editorial;
+            private string autor;
+
+            public Libro()
+            {
+                id = 150918;
+                categoria = "Novela";
+                editorial = "Santillana";
+                autor = "Miguel De Cervantes";
+            }
+
+            public Libro(int id, string categoria, string editorial, string autor)
+            {
+                this.id = id;
+                this.categoria = categoria;
+                this.editorial = editorial;
+                this.autor = autor;
+            }
+
+            public int getId()
+            {
+                return id;
+            }
+
+            public void setId(int id)
+            {
+                this.id = id;
+            }
+
+            public string getCategoria()
+            {
+                return categoria;
+            }
+
+            public void setCategoria(string categoria)
+            {
+                this.categoria = categoria;
+            }
+
+            public string getEditorial()
+            {
+                return editorial;
+            }
+
+            public void setEditorial(string editorial)
+            {
+                this.editorial = editorial;
+            }
+
+            public string getAutor()
+            {
+                return autor;
+            }
+
+            public void setAutor(string autor)
+            {
+                this.autor = autor;
+            }
+        }
+
+
+        public class Program
+        {
+            public static void Libro()
+            {
+                Libro miLibro = new Libro();
+                miLibro.setId(150918);
+                miLibro.setCategoria("Novela");
+                miLibro.setEditorial("Santillana");
+                miLibro.setAutor("Miguel De Cervantes");
+
+                Console.WriteLine("Id:150918 " + miLibro.getId());
+                Console.WriteLine("Categoría: Novela " + miLibro.getCategoria());
+                Console.WriteLine("Editorial: Santillana" + miLibro.getEditorial());
+                Console.WriteLine("Autor: Miguel De Cervantes " + miLibro.getAutor());
+            }
+        }
+
+    
 }
-
-public class Libro
-{
-    private int id;
-    private string categoria;
-    private string editorial;
-    private string autor;
-
-    public Libro()
-    {
-        id = 0;
-        categoria = "";
-        editorial = "";
-        autor = "";
-    }
-
-    public Libro(int id, string categoria, string editorial, string autor)
-    {
-        this.id = id;
-        this.categoria = categoria;
-        this.editorial = editorial;
-        this.autor = autor;
-    }
-
-    public int getId()
-    {
-        return id;
-    }
-
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
-    public string getCategoria()
-    {
-        return categoria;
-    }
-
-    public void setCategoria(string categoria)
-    {
-        this.categoria = categoria;
-    }
-
-    public string getEditorial()
-    {
-        return editorial;
-    }
-
-    public void setEditorial(string editorial)
-    {
-        this.editorial = editorial;
-    }
-
-    public string getAutor()
-    {
-        return autor;
-    }
-
-    public void setAutor(string autor)
-    {
-        this.autor = autor;
-    }
-}
-
-public class Program
-{
-    public static void Main()
-    {
-        Libro miLibro = new Libro();
-        miLibro.setId(1);
-        miLibro.setCategoria("Novela");
-        miLibro.setEditorial("Editorial XYZ");
-        miLibro.setAutor("Juan Pérez");
-
-        Console.WriteLine("Id: " + miLibro.getId());
-        Console.WriteLine("Categoría: " + miLibro.getCategoria());
-        Console.WriteLine("Editorial: " + miLibro.getEditorial());
-        Console.WriteLine("Autor: " + miLibro.getAutor());
-    }
-}
-
 
--- Lab.Models/Grupo7/Prestamo.cs
+++ Lab.Models/Grupo7/Prestamo.cs
@@ -1,8 +1,52 @@
 ﻿using System;
 
-public class Class1
+public class PrestamoDeLibro
 {
-	public Class1()
-	{
-	}
+    private DateTime fechaPréstamo;
+    private DateTime fechaDevolución;
+    private int idLibro;
+    private string cliente;
+
+    public DateTime FechaPréstamo
+    {
+        get { return fechaPréstamo; }
+        set { fechaPréstamo = value; }
+    }
+
+    public DateTime FechaDevolución
+    {
+        get { return fechaDevolución; }
+        set { fechaDevolución = value; }
+    }
+
+    public int IDLibro
+    {
+        get { return idLibro; }
+        set { idLibro = value; }
+    }
+
+    public string Cliente
+    {
+        get { return cliente; }
+        set { cliente = value; }
+    }
+
+    public static void Préstamo(string[] args)
+    {
+        
+        PrestamoDeLibro prestamo = new PrestamoDeLibro();
+
+       
+        prestamo.FechaPréstamo = DateTime.Now;
+        prestamo.FechaDevolución = DateTime.Now.AddDays(14);
+        prestamo.IDLibro = 150418;
+        prestamo.Cliente = "Luis Gómez";
+
+        
+        Console.WriteLine("Fecha de Préstamo: " + prestamo.FechaPréstamo);
+        Console.WriteLine("Fecha de Devolución: " + prestamo.FechaDevolución);
+        Console.WriteLine("ID del Libro: " + prestamo.IDLibro);
+        Console.WriteLine("Cliente: " + prestamo.Cliente);
+    }
 }
+
