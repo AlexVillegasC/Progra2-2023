d20786b74a37fe9b696ea885816dd1cf4beae0ee
1van2004 <ivanaa2004@hotmail.com>
2023-05-12 14:39:34 -0600

    Grupo Gama, CLI

---
+++ GrupoGamma.CLI/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Lab.Models.GrupoGamma;
+
+Empresa empresa = new Empresa();
+
+empresa.Cliente = new List<Cliente>();
+Cliente primerCliente = new Cliente();
+primerCliente.Nombre = "Edgardo Mora";
+primerCliente.Id = 504320687;
+primerCliente.Telefono = 26561482;
+empresa.Cliente.Add(primerCliente);
+Console.WriteLine(empresa.Cliente[0]);
+
+empresa.Empleado = new List<Empleado>();
+Empleado primerEmpleado = new Empleado();
+primerEmpleado.id = 403240675;
+primerEmpleado.Nombre_emp = "José Hernandez";
+primerEmpleado.Telefono_emp = 62613482;
+primerEmpleado.campo = "Programacion";
+empresa.Empleado.Add(primerEmpleado);
+Console.WriteLine(empresa.Empleado[0]);
+
+empresa.Software = new List<Software>();
+Software primerSoftware = new Software();
+primerSoftware.Nombre_app = "EasyMap";
+primerSoftware.funcion = "Creación de mapas conseptuales";
+primerSoftware.Requerimientos = "Existir (opcional)";
+primerSoftware.fecha_inicio = DateTime.Now;
+primerSoftware.fecha_entrega = new DateTime(2024, 12, 23);
+empresa.Software.Add(primerSoftware);
+Console.WriteLine(empresa.Software[0]);
--- Lab.Models/GrupoGamma/Cliente.cs
+++ Lab.Models/GrupoGamma/Cliente.cs
@@ -4,5 +4,5 @@ public class Cliente
 {
     public string Nombre { get; set; }
     public int Id { get; set; }
-    public string Telefono { get; set; }
+    public int Telefono { get; set; }
 }
