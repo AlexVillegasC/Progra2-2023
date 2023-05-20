b230ebf3181ead94cf9a8d2422998323a2f4c344
pity05 <diazpri4@gmail.com>
2023-05-19 21:30:32 -0600

    ActualizacionProgram

--- Lab.Models/Grupo9/Envios.cs
+++ Lab.Models/Grupo9/Envios.cs
@@ -7,28 +7,8 @@ using Lab.Models.Grupo9;
 
 public class Envio
 {
-    public string Paquete { get; set; }
-    public string Status { get; set; }
-    public string Cliente { get; set; }
+    public  List <Paquetes> Paquetes{ get; set; }
+    public List <Estatus> Status { get; set; }
+    public List <Cliente> Cliente { get; set; }
 
-    public Envio(string paquete, string status, string cliente)
-    {
-        Paquete = paquete;
-        Status = status;
-        Cliente = cliente;
-    }
-
-    public void ActualizarStatus(string nuevoStatus)
-    {
-        Status = nuevoStatus;
-        Console.WriteLine("El estado del envío se ha actualizado a: " + nuevoStatus);
-    }
-
-    public void MostrarInformacion()
-    {
-        Console.WriteLine("Información del envío:");
-        Console.WriteLine("Paquete: " + Paquete);
-        Console.WriteLine("Estado: " + Status);
-        Console.WriteLine("Cliente: " + Cliente);
-    }
 }
