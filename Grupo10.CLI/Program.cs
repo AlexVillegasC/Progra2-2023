df00b5d7002461021f7bdd5c27ed497a0d41e0e5
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2023-05-22 17:58:41 -0600

    Newtonsoft.json

--- Grupo10.CLI/Program.cs
+++ Grupo10.CLI/Program.cs
@@ -1,14 +1,25 @@
 ﻿using Lab.Models.Grupo10;
 using Newtonsoft.Json;
+//using System.Text.Json;
 
 
 ReportePlanilla GetReportePlanilla()
 {
     string json = File.ReadAllText("C:/Users/Suare/RiderProjects/Progra2/Infrastructure.Shared/DB/grupo10-reporteplanilla.json");
+
     Empleado[]? listEmpleados = JsonConvert.DeserializeObject<Empleado[]>(json);
     ReportePlanilla reportePlanilla = new ReportePlanilla();
     reportePlanilla.Empleados = listEmpleados != null ? new List<Empleado>(listEmpleados) : new List<Empleado>();
+
     return reportePlanilla;
+    
+    /*string json = File.ReadAllText("C:/Users/Suare/RiderProjects/Progra2/Infrastructure.Shared/DB/grupo10-reporteplanilla.json");
+
+    Empleado[]? listEmpleados = JsonSerializer.Deserialize<Empleado[]>(json);
+    ReportePlanilla reportePlanilla = new ReportePlanilla();
+    reportePlanilla.Empleados = listEmpleados != null ? new List<Empleado>(listEmpleados) : new List<Empleado>();
+
+    return reportePlanilla;*/
 }
 
 ReportePlanilla reportePlanilla = GetReportePlanilla();
