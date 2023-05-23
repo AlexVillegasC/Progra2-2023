095785f6b429f972e240a61fdfc75c55cf7fe506
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2023-05-22 19:20:33 -0600

    Agregar cambios de empleado

--- Grupo10.CLI/Program.cs
+++ Grupo10.CLI/Program.cs
@@ -1,25 +1,17 @@
 ﻿using Lab.Models.Grupo10;
 using Newtonsoft.Json;
-//using System.Text.Json;
-
+using System.Text.Json;
 
 ReportePlanilla GetReportePlanilla()
+
 {
-    string json = File.ReadAllText("C:/Users/Suare/RiderProjects/Progra2/Infrastructure.Shared/DB/grupo10-reporteplanilla.json");
+    string json = File.ReadAllText("C:/Users/Suare/RiderProjects/Progra2/Infrastructure.Shared/DB/grupo10-Empleado.json");
 
     Empleado[]? listEmpleados = JsonConvert.DeserializeObject<Empleado[]>(json);
     ReportePlanilla reportePlanilla = new ReportePlanilla();
     reportePlanilla.Empleados = listEmpleados != null ? new List<Empleado>(listEmpleados) : new List<Empleado>();
 
     return reportePlanilla;
-    
-    /*string json = File.ReadAllText("C:/Users/Suare/RiderProjects/Progra2/Infrastructure.Shared/DB/grupo10-reporteplanilla.json");
-
-    Empleado[]? listEmpleados = JsonSerializer.Deserialize<Empleado[]>(json);
-    ReportePlanilla reportePlanilla = new ReportePlanilla();
-    reportePlanilla.Empleados = listEmpleados != null ? new List<Empleado>(listEmpleados) : new List<Empleado>();
-
-    return reportePlanilla;*/
 }
 
 ReportePlanilla reportePlanilla = GetReportePlanilla();
@@ -30,6 +22,6 @@ foreach(var empleado in reportePlanilla.Empleados)
     Console.WriteLine("El nombre del empleado es:" + empleado.Nombre);
     Console.WriteLine("Su departamento es: " + empleado.Departamento?.NombreDepartamento);
     Console.WriteLine("Su roll es: " + empleado.Salario?.Roll);
-    Console.WriteLine("Su salario es: " + empleado.Salario?.Monto);
+    Console.WriteLine("Su salario es: " + empleado.Salario?.Monto + "$");
     Console.WriteLine("");
 }
--- Lab.Models/Grupo10/ReportePlanilla.cs
+++ Lab.Models/Grupo10/ReportePlanilla.cs
@@ -3,9 +3,9 @@
 using System.Collections.Generic;
 public class ReportePlanilla
 {
-    public List<Empleado> Empleados;
-    public List<Departamento> Departamentos;
-    public List<Salario> Salarios;
+    public List<Empleado> Empleados { get;  set; }
+    public List<Departamento> Departamentos { get;  set; }
+    public List<Salario> Salarios { get;  set; }
 
     public ReportePlanilla()
     {
@@ -13,7 +13,11 @@ public class ReportePlanilla
         Departamentos = new List<Departamento>();
         Salarios = new List<Salario>();
     }
-
+    public IEnumerable<Empleado> BuscarEmpleado(int id)
+    {
+        return Empleados.Where(empleado => empleado.Id == id);
+    }
+    
     public void Contratar(Empleado empleado)
     {
         Empleados.Add(empleado);
@@ -23,26 +27,11 @@ public class ReportePlanilla
     {
         Empleados.Remove(empleado);
     }
+    
+    //LINQ (Language-Integrated Query) es una característica de C# que proporciona una forma unificada de consultar
+    //y manipular datos desde diversas fuentes de datos, como colecciones, bases de datos, servicios web, etc.
+    public double SalarioTotal => Empleados.Sum(empleado => empleado.Salario?.Monto ?? 0);
 
-    public Empleado? BuscarEmpleado(int id)
-    {
-        return Empleados.Find(empleado => empleado.Id == id);
-    }
-
-    public double CalcularSalarioTotal()
-    {
-        double totalSalario = 0;
-        foreach (Empleado empleado in Empleados)
-        {
-            totalSalario += empleado.ObtenerSalario();
-        }
-        return totalSalario;
-    }
-
-    public double CalcularSalarioPromedio()
-    {
-        double totalSalario = CalcularSalarioTotal();
-        int numEmpleados = Empleados.Count;
-        return totalSalario / numEmpleados;
-    }
+    public double SalarioPromedio => Empleados.Any() ? SalarioTotal / Empleados.Count : 0;
+    
 }
