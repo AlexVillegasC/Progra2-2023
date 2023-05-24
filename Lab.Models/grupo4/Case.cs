968e479b980c3fca0ce1e8b8db86728cab01673d
YoilinCastrillo <yoilinc7alfaro@gmail.com>
2023-05-23 23:55:11 -0600

    Clase clase

--- Lab.Models/grupo4/Case.cs
+++ Lab.Models/grupo4/Case.cs
@@ -8,8 +8,37 @@ namespace Lab.Models.Grupo4
 {
     public class Case
     {
-        public CPU CPU { get; set; }
-        public T_Grafica T_Grafica { get; set; }
-        public Almacenamiento Almacenamiento { get; set; }
+        int Id;
+        string Nombre;
+        double Tamanno;
+        string Color;
+        decimal Costo;
+        string Forma;
+
+        public Case(int id, string nombre, double tamanno, string color, decimal costo, string forma)
+        {
+            this.Id = id;
+            Nombre = nombre;
+            Tamanno = tamanno;
+            Color = color;
+            Costo = costo;
+            Forma = forma;
+        }
+        public int getId() { return Id; }
+        public string getNombre() { return Nombre; }
+        public double getTamanno() { return Tamanno; }
+        public string getColor() { return Color; }
+        public decimal getCosto() { return Costo; }
+        public string getForma() { return Forma; }
+
+        public void setId(int id) { this.Id = id; }
+        public void setNombre(string nombre) { this.Nombre = nombre; }
+        public void setTamanno(double tamanno) { this.Tamanno = tamanno; }
+        public void setColor(string color) { this.Color = color; }
+        public void setCosto(decimal costo) { this.Costo = costo; }
+        public void setForma(string forma) { this.Forma = forma; }
+
+
+
     }
 }
