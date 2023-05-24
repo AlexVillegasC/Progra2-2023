d9876baf8603fac1513ffd44ca07c91e99650af0
Axel Ramos <axelramos082@gmail.com>
2023-05-24 14:55:29 -0600

    Almace

---
+++ Lab.Models/GrupoGamma/Empresa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Models.GrupoGamma
+{
+    public class Empresa
+    {
+        private Empleado Empleado { get; set; }
+        private Software Sofware { get; set; }
+        private Cliente Cliente { get; set; }
+        private String Nombre { get; set;}
+        private String Direccion { get; set;}
+        private int telefono { get; set;}
+        private String Correo { get; set;}
+    }
+
+
+
+    }
