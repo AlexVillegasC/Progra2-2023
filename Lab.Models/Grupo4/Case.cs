8859cb42ea2c27126baa7ddd5c1cda016d97ecf2
YoilinCastrillo <yoilinc7alfaro@gmail.com>
2023-05-23 22:13:10 -0600

    Cambios para .CLI

---
+++ Lab.Models/Grupo4/Case.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Models.Grupo4
+{
+    public class Case
+    {
+        public CPU CPU { get; set; }
+        public T_Grafica T_Grafica { get; set; }
+        public Almacenamiento Almacenamiento { get; set; }
+    }
+}
