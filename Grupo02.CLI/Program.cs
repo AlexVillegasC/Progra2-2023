0f71e913b7ae07b9697779cfc3bc5cb545a07d5a
Victor Jimenez <Victor Jimenez@DESKTOP-F5IKB38>
2023-06-05 08:17:41 -0600

    Agregacion del json a la CLI

--- Grupo02.CLI/Program.cs
+++ Grupo02.CLI/Program.cs
@@ -6,13 +6,17 @@ using System.Collections.Generic;
 
 Inventario GetMyInventario()
 {   
-    var virtualPath = "../../../../Infrastructure.Shared/DB/Grupo02-Frutas.json";
-    FileRepository fileRepo = new FileRepository();
+    var FrutasVirtualPath = "../../../../Infrastructure.Shared/DB/Grupo02-Frutas.json";
+	var BebidasVirtualPath = "../../../../Infrastructure.Shared/DB/Grupo02-Bebidas.json";
+	FileRepository fileRepo = new FileRepository();
 
-    List<Frutas> frutas = fileRepo.ReadJsonFileAsync<List<Frutas>>(virtualPath).Result;
+    List<Frutas> frutas = fileRepo.ReadJsonFileAsync<List<Frutas>>(FrutasVirtualPath).Result;
+	List<Bebidas> bebidas = fileRepo.ReadJsonFileAsync<List<Bebidas>>(BebidasVirtualPath).Result;
 
-    Inventario inventario = new Inventario();
+	Inventario inventario = new Inventario();
     inventario.Frutas = frutas;
+    inventario.Bebidas = bebidas;
+
     return inventario; 
 }
  
@@ -23,4 +27,9 @@ foreach (var frutas in myInventario.Frutas)
 {
     Console.WriteLine(frutas.Cantidad);
     Console.WriteLine(frutas.TipodeFruta);
+}
+foreach (var bebidas in myInventario.Bebidas)
+{
+	Console.WriteLine(bebidas.Marcas);
+	Console.WriteLine(bebidas.TipodeBebida);
 }
