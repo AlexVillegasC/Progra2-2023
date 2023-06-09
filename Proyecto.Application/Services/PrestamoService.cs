e795143f75f311dc6d4816a501f583189d680639
PinedaCR10 <emmanuelplay2233@gmail.com>
2023-06-08 22:38:52 -0600

    Service

---
+++ Proyecto.Application/Services/PrestamoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Application.Services
+{
+    internal class PrestamoService
+    {
+    }
+}
