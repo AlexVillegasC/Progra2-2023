904607677f7d421e872e2b845a384c7f78341b75
AshVargas <vargasashly260@gmail.com>
2023-06-08 19:32:20 -0600

    Arreglo de la clase Salario

--- Lab.Models/Grupo10/Salario.cs
+++ Lab.Models/Grupo10/Salario.cs
@@ -2,20 +2,8 @@
 
 public class Salario
 {
-<<<<<<< HEAD
-    public int sueldo_base { get; set; }
-    public int horas_extra { get; set; }
-    public int bonificaciones { get; set; }
-
-    public double CalcularSalarioTotal()
-    {
-        return CalcularSalarioTotal();
-    }
-
-}
-=======
     public int Id { get; set; }
     public string Roll { get; set; }
     public int Monto { get; set; }
 }
->>>>>>> origin/Grupo10
+
