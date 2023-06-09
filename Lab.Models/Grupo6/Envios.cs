8e0d26786c5660f7587fe9911360b82047034f80
Sam2999 <albertsam29@gmail.com>
2023-06-09 13:48:51 -0600

    Envios Actualizado

--- Lab.Models/Grupo6/Envios.cs
+++ Lab.Models/Grupo6/Envios.cs
@@ -2,14 +2,13 @@
 {
     private Cotizaciones cotizaciones { get; set; }
     private Seguimiento seguimiento { get; set; }
-    private Paquete paquete { get; set; }
+    private Clientes clientes { get; set; }
 
     public Envios()
     {
         cotizaciones = new Cotizaciones();
         seguimiento = new Seguimiento();
-        paquete = new Paquete();
-
+        clientes = new Clientes();
     }
 
-}
+} 
