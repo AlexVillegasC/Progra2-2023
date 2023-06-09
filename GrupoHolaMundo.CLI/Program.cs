40d099fcdc56b055da53e5650841d34fa857322e
Leonides2 <114005429+Leonides2@users.noreply.github.com>
2023-06-09 13:09:08 -0600

    a

--- GrupoHolaMundo.CLI/Program.cs
+++ GrupoHolaMundo.CLI/Program.cs
@@ -1,49 +1,103 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using Infrastructure.Shared.Files;
 using Lab.Models.GrupoHolaMundo;
-using grupoHolaMundo = Lab.Models.GrupoHolaMundo;
-
-Alquiler alquiler = new Alquiler();
-alquiler.cliente = new List<Cliente>();
-
-string nombre, apellidos;
-int cedula;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 
-for (int i = 0; i < 5; i++)
+Alquiler GetDatosAlquiler()
 {
-    Console.WriteLine("Ingrese los datos de los clientes en el siguiente orden (nombre, apellidos, cedula)", "", "Cliente "+ (i+1));
 
-    nombre = Console.ReadLine();
-    apellidos = Console.ReadLine();
-    cedula = int.Parse(Console.ReadLine());
-    Cliente cliente = new Cliente() { Nombre = nombre, Apellidos = apellidos, Cedula = cedula };
-    alquiler.cliente.Add(cliente);
-    Console.Clear();
- }
+    var casasVirtualPath = "../../../../Infrastructure.Shared/DB/grupoHolaMundo-casas.json";
+    var clientesVirtualPath = "../../../../Infrastructure.Shared/DB/grupoHolaMundo-clientes.json";
+    var paquetesVirtualPath = "../../../../Infrastructure.Shared/DB/grupoHolaMundo-paquetes.json";
 
+    FileRepository fileRepo = new FileRepository();
 
+    List<Casa> casas = fileRepo.ReadJsonFileAsync<List<Casa>>(casasVirtualPath).Result;
+    List<Cliente> clientes = fileRepo.ReadJsonFileAsync<List<Cliente>>(clientesVirtualPath).Result;
+    List<Paquetes> paquetes = fileRepo.ReadJsonFileAsync<List<Paquetes>>(paquetesVirtualPath).Result;
 
+    Alquiler alquiler = new Alquiler();
+    alquiler.casa = casas;
+    alquiler.cliente = clientes;
+    alquiler.paquete = paquetes;
 
-for (int i = 0; i < 5; i++)
-{
-    int decision;
-
-    alquiler.calcularTotalBase(i);
-
-    Console.WriteLine("Cliente " + (i + 1), " ", "Casa " + (i + 1), " ", "Desea agregar un paquete de internet? (1 paran si, 0 para no)");
-    decision = int.Parse(Console.ReadLine());
-    if (decision == 1)
-    {
-
-    }
-    else
-    {
-
-    }
-
-    Console.WriteLine(alquiler.cliente[i]);
-    Console.WriteLine(alquiler.casa[i]);
+    return alquiler;
 }
 
+Alquiler myAlquiler = GetDatosAlquiler();
+
+Console.WriteLine("Casa:");
+
+foreach (var casa in myAlquiler.casa)
+{
+    Console.WriteLine(casa.habitaciones);
+    Console.WriteLine(casa.banos);
+    Console.WriteLine(casa.comedor);
+    Console.WriteLine(casa.piscina);
+}
+
+Console.WriteLine("Cliente:");
+foreach (var clientes in myAlquiler.cliente)
+{
+    Console.WriteLine(clientes.nombre);
+    Console.WriteLine(clientes.apellidos);
+    Console.WriteLine(clientes.cedula);
+}
+
+Console.WriteLine("Paquetes:");
+
+foreach (var paquetes in myAlquiler.paquete)
+{
+    Console.WriteLine(paquetes.velocidad);
+    Console.WriteLine(paquetes.precio);
+}
+
+
+
+
+//Alquiler alquiler = new Alquiler();
+//alquiler.cliente = new List<Cliente>();
+
+//string nombre, apellidos;
+//int cedula;
+
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine("Ingrese los datos de los clientes en el siguiente orden (nombre, apellidos, cedula)", "", "Cliente "+ (i+1));
+
+//    nombre = Console.ReadLine();
+//    apellidos = Console.ReadLine();
+//    cedula = int.Parse(Console.ReadLine());
+//    Cliente cliente = new Cliente() { Nombre = nombre, Apellidos = apellidos, Cedula = cedula };
+//    alquiler.cliente.Add(cliente);
+//    Console.Clear();
+// }
+
+
+
+
+//for (int i = 0; i < 5; i++)
+//{
+//    int decision;
+
+//    alquiler.calcularTotalBase(i);
+
+//    Console.WriteLine("Cliente " + (i + 1), " ", "Casa " + (i + 1), " ", "Desea agregar un paquete de internet? (1 paran si, 0 para no)");
+//    decision = int.Parse(Console.ReadLine());
+//    if (decision == 1)
+//    {
+
+//    }
+//    else
+//    {
+
+//    }
+
+//    Console.WriteLine(alquiler.cliente[i]);
+//    Console.WriteLine(alquiler.casa[i]);
+//}
+
 //Agregar implementación de clases en este archivo. Revisar el codigo comentado en alquiler commo guía
+
