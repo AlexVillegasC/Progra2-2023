f70b9bef3ef66471cdec6a21c5bee9dd294e408b
PinedaCR10 <emmanuelplay2233@gmail.com>
2023-06-08 23:27:41 -0600

    Service

--- Proyecto.Application/Services/PrestamoService.cs
+++ Proyecto.Application/Services/PrestamoService.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Lab.Models.Grupo7;
 
-namespace Proyecto.Application.Services
+namespace Proyecto.Application.Services;
+
+public class PrestamoService
 {
-    internal class PrestamoService
+    private readonly IPrestamoRepository _prestamoRepository;
+
+
+    public PrestamoService(IPrestamoRepository prestamoRepository)
     {
+         _prestamoRepository = prestamoRepository;
+    }
+
+    public async Task<Prestamo> ListaPrestamo()
+    {
+        List<Libro> Libros = await _prestamoRepository.GetLibros();
+        List<Libro> Clientes = await _prestamoRepository.GetClientes();
+        List<Libro> Tarifas = await _prestamoRepository.GetTarifas();
+
+
+        Prestamo Prestamo = new Prestamo()
+        {
+            Libros = Libros,
+            Clientes = Clientes,
+            Tarifas = Tarifas
+        };
+
+        return Prestamo;
     }
 }
