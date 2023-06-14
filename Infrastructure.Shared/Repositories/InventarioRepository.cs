5591fe552eefb02982438f698ffc8a5e7f0ed6bc
Victor Jimenez <Victor Jimenez@DESKTOP-F5IKB38>
2023-06-14 10:27:16 -0600

    Proyecto 2

---
+++ Infrastructure.Shared/Repositories/InventarioRepository.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Shared.Files;
+using Lab.Models.Grupo2;
+
+namespace Infrastructure.Shared.Repositories;
+
+public class InventarioRepository : FileRepository, I_InventarioRepository
+{
+    private const string FrutasVirtualPath = "../Infrastructure.Shared/DB/Grupo02-Frutas.json";
+    private const string BebidasVirtualPath = "../Infrastructure.Shared/DB/Grupo02-Bebidas.json";
+
+    public async Task<List<Frutas>> GetFrutas()
+    {
+        return await ReadJsonFileAsync<List<Frutas>>(FrutasVirtualPath);
+    }
+
+    public async Task<List<Bebidas>> GetBebidas()
+    {
+        return await ReadJsonFileAsync<List<Bebidas>>(BebidasVirtualPath);
+    }
+
+
+}
