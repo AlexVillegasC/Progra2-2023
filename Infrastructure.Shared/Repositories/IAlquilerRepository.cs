a9cf47b00711a591df2b190659cb513d8f095a4d
Leonides2 <114005429+Leonides2@users.noreply.github.com>
2023-06-15 20:33:13 -0600

    Inicio proyecto2

---
+++ Infrastructure.Shared/Repositories/IAlquilerRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using Lab.Models.GrupoHolaMundo;
+
+namespace Infrastructure.Shared.Repositories;
+
+public interface IAlquilerRepository
+{
+    Task<List<Casa>> GetCasas();
+    Task<List<Cliente>> GetClientes();
+    Task<List<PaqueteInternet>> GetPaquetes();
+
+}
