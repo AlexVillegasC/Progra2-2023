431a96ba9f451cbea925c41452e6641f09bc3718
JimenaJG <jime112010@hotmail.com>
2023-06-16 15:41:15 -0600

    AAA

---
+++ Lab.Models/Grupo3/Memoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Models.Grupo3
+{
+    public class Memoria
+    {
+
+        public int EspacioTotal { get; set; }
+        public int EspacioUsado { get; set; }
+        public int EspacioLibre { get; set; }
+
+    }
+}
