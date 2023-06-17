956468b8d92e79a794a446ef7bb5966606e9036c
Sam2999 <albertsam29@gmail.com>
2023-06-16 23:59:35 -0600

    Grupo6 API: Repositories, Services, Controller.

---
+++ Grupo6.API/Controllers/EnviosController.cs
@@ -0,0 +1,23 @@
+﻿using Lab.Models.Grupo6;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto.Application.Services;
+
+namespace Grupo6.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class EnviosController : Controller
+    {
+        private readonly EnviosService _enviosService;
+        public EnviosController(EnviosService enviosService) 
+        {
+            _enviosService = enviosService;
+        }
+
+        [HttpGet(Name = "GetEnvios")]
+        public Task<Envios> Get()
+        {
+            return _enviosService.GetMyEnvios();
+        }
+    }
+}
