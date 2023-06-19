5a0ec9486da5802c8a9370c23f00f61363b808f1
Alex Villegas <avillegas@dragonspears.com>
2023-06-19 15:39:17 -0600

    Merge con Main

--- Grupo4.CLI/Program.cs
+++ Grupo4.CLI/Program.cs
@@ -1,32 +1,31 @@
 ﻿using Infrastructure.Shared.Files;
 using Lab.Models.Grupo4;
-using Newtonsoft.Json;
 
-        CPU GetCase()
-        {
-            // RUTA CASE
-            var virtualpathcase = "../../../../Infrastructure.Shared/DB/Grupo4-Case.json";
-            FileRepository fileRepoCase = new FileRepository();
-            
-             //RUTA ALMACENAMIENTO
-            var PathStorage = "../../../../Infrastructure.Shared/DB/Grupo4-Almacenamiento.json";
-            FileRepository fileRepoStorage = new FileRepository();
+CPU GetCase()
+{
+    // RUTA CASE
+    var virtualpathcase = "../../../../Infrastructure.Shared/DB/Grupo4-Case.json";
+    FileRepository fileRepoCase = new FileRepository();
 
-             //RUTA T_GRAFICA 
-            var PathGraph = "../../../../Infrastructure.Shared/DB/Grupo4-T_Grafica.json";
-            FileRepository fileRepoGraph = new FileRepository();
+    //RUTA ALMACENAMIENTO
+    var PathStorage = "../../../../Infrastructure.Shared/DB/Grupo4-Almacenamiento.json";
+    FileRepository fileRepoStorage = new FileRepository();
+
+    //RUTA T_GRAFICA 
+    var PathGraph = "../../../../Infrastructure.Shared/DB/Grupo4-T_Grafica.json";
+    FileRepository fileRepoGraph = new FileRepository();
 
 
-           List<Case> cases = fileRepoCase.ReadJsonFileAsync<List<Case>>(virtualpathcase).Result;
-           List<Almacenamiento> almacenamientos = fileRepoStorage.ReadJsonFileAsync<List<Almacenamiento>>(PathStorage).Result;
-           List<T_Grafica> graficas = fileRepoGraph.ReadJsonFileAsync<List<T_Grafica>>(PathGraph).Result;
+    List<Case> cases = fileRepoCase.ReadJsonFileAsync<List<Case>>(virtualpathcase).Result;
+    List<Almacenamiento> almacenamientos = fileRepoStorage.ReadJsonFileAsync<List<Almacenamiento>>(PathStorage).Result;
+    List<T_Grafica> graficas = fileRepoGraph.ReadJsonFileAsync<List<T_Grafica>>(PathGraph).Result;
 
-           CPU cpu = new CPU ();
-           cpu.Cases = cases;
-           cpu.Almacenamientos = almacenamientos;
-           cpu.TarjetaGraficas = graficas;
-           return cpu;
-        }
+    CPU cpu = new CPU();
+    cpu.Cases = cases;
+    cpu.Almacenamientos = almacenamientos;
+    cpu.TarjetaGraficas = graficas;
+    return cpu;
+}
 
 
 CPU cpu = GetCase();
